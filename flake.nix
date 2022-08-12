224fb8dead9b2e32376d55e21301f5802cea954e
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-08-12 18:20:44 -0500

    init commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  # inputs.nixpks.url = "github:nixos/nixpkgs?ref=nixos-22.05";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs =
+    { self
+    , nixpkgs
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+
+      pythonPackages = with pkgs.python310Packages; [
+        tkinter
+      ];
+    in
+    rec {
+      devShell.${system} = pkgs.mkShell {
+
+        nativeBuildInputs = with pkgs; [
+          python310
+        ] ++ pythonPackages;
+
+        shellHook = ''
+          if [ -e .venv ]; then
+              source .venv/bin/activate
+          fi
+          echo "Entering python arcade shell..."
+        '';
+      };
+    };
+}
