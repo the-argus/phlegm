bdb59e8d8b9ec6cc41a6e1474a1058b876d16675
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-08-15 13:01:14 -0500

    cooldown button class, and ttk

--- run.py
+++ run.py
@@ -1,4 +1,49 @@
 import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import font
+
+
+class GameObject:
+    def __init__(self):
+        self.tags = {}
+
+
+class CooldownButton(ttk.Button, GameObject):
+    """A version of button which comes paired with a progressbar."""
+    def __init__(self, parent, **kwargs):
+        """
+            parent: Tk root
+            cooldown: integer, how long progressbar takes before allowing
+                another press
+        """
+        # remove keyword arguments that relate to cooldown functionality
+        self.command = kwargs.pop("command")
+        self.cooldown = int(kwargs.pop("cooldown"))
+
+        # replace cooldown with our custom press method
+        kwargs["command"] = self.press
+
+        # complete initialization of button
+        super().__init__(parent, **kwargs)
+        GameObject.__init__(self)
+
+        # internal variable declaration
+        self._timer = 0
+
+        # initialize component(s)
+        self.tags["update"] = self.update
+
+    def press(self):
+        """Callback when button is pressed."""
+        if self._timer > 0:
+            return
+        self.command()
+        self._timer = self.cooldown
+
+    def update(self):
+        """Callback every frame."""
+        if self._timer > 0:
+            self._timer -= 1
 
 
 class Game:
@@ -23,6 +68,10 @@ class Game:
 
 
 main_window = tk.Tk()
+default_family = f"\"{font.families()[0]}\""
+def_font = font.nametofont("TkDefaultFont")
+def_font.config(size=19, family=default_family)
+# main_window.option_add('*Font', f"{default_family} 19")
 main = tk.Canvas(main_window,
                  width=100,
                  height=100)
@@ -34,8 +83,8 @@ kill = tk.Button(main_window,
                  width=100,
                  command=main_window.destroy)
 # add all the buttons
-kill.pack()
-game.slime_button.pack()
+kill.pack(padx=5, pady=5)
+game.slime_button.pack(padx=5, pady=5)
 
 # open floating window
 main_window.attributes('-type', 'dialog')
